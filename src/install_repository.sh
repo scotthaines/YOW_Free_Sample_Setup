128baa3eb351a1da10f4d07fc0c8b00cd1eb35de
Scott Haines <friedbook@gmail.com>
2016-03-02 07:40:54 -0600

    Now Github is used, "git clone https://github.com/scotthaines/YOW_Free_Sample.git repository". The debug test line to pause is now after the collection of the clone results to collect results.

--- src/install_repository.sh
+++ src/install_repository.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 # file: install_repository.sh
 # created: 2016 01 31, Scott Haines
-# edit: 02 Scott Haines
-# date: 2016 01 31
+# edit: 04 Scott Haines
+# date: 2016 03 02
 # This shell script runs in Git's bash. It installs the YOW Free Sample Git 
 # repository by cloning it into the directory named repository in the
 # current working directory.
 # .
 # Run the next line to see commands and expand variables before running them.
 # set -x
-git clone c:/projects/yow_free_sample repository
-
-# The following is a commented out pause for debugging.
-# read -p "Pause. Press enter to continue."
+# The following line is a commented alternate line for debug testing.
+# git clone c:/projects/yow_free_sample repository
+git clone https://github.com/scotthaines/YOW_Free_Sample.git repository
 
 # Save the clone command's return code.
 # We do not want the return code of the 'read' below.
 SH_RETURN_CODE=$?
+
+# The following is a commented out pause for debug testing.
+# read -p "Pause. Press enter to continue."
+
 # If there is no error creating the repository
 if [ $SH_RETURN_CODE -eq 0 ]
 then
     # Remove the remote origin to avoid unintended pushes to YOW Free Sample
-	# on GitHub.
-	cd repository
-	git remote remove origin
-	cd ..
+    # on GitHub.
+    cd repository
+    git remote remove origin
+    cd ..
 fi
 # Run the next line to view and debug this shell script.
 # read -p "Press [Enter] key to start again."
